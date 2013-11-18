eadabdc33e5967a4e56b1f3c668a1a8f0b1f24ab
Ajay <ajsolanky@gmail.com>
2013-11-17 19:16:59 -0800

    finished mod.s

--- test-files/assem/mod.s
+++ test-files/assem/mod.s
@@ -1,7 +1,19 @@
 MOD:	lui $r0 0
-		lw $r1 0($r0)
-		lw $r2 1($r0)
-		addi $r2 $r2 -1
-		and $r1 $r1 $r2
-		sw $r1 2($r0)
+		sw $r3 2($r0)
+		lw $r1 1($r0)
+		slt $r2 $r1 $r0
+		beq $r2 $r0 rest
+		sub $r1 $r0 $r1
+rest: 
+		lw $r0 0($r0)
+continue:
+		slt $r2 $r0 $r1
+		lui $r3 0
+		bne $r2 $r3 return
+		sub $r0 $r0 $r1
+		j continue
+return:
+		lui $r1 0
+		lw $r3 2($r1)
+		sw $r0 0($r1)
 		jr $r3
